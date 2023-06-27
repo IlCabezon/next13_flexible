a96a306901fa674a4aa411233485404b05513744
IlCabezon <aguscastro2014.ac@gmail.com>
2023-06-27 15:35:00 -0300

    [FT] globals setup

---
+++ src/common.types.ts
@@ -0,0 +1,11 @@
+import { User, Session } from "next-auth";
+
+export type FormState = {};
+
+export interface ProjectInterface {}
+
+export interface UserProfile {}
+
+export interface SessionInterface extends Session {}
+
+export interface ProjectForm {}
